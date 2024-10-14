1e4e8907a70722516b32fd69e8f0d6b83145d594
akramthedev <seasonedwebdev@gmail.com>
2024-10-14 12:18:39 +0100

    dsfwdv

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiter as CacheRateLimiter;
+use Illuminate\Http\Request;
+use Illuminate\Cache\RateLimiter as RateLimiterFacade;
+use Illuminate\Routing\Middleware\ThrottleRequests;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Define rate limits
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(200);  
+        });
+
+        RateLimiter::for('second', function (Request $request) {
+            return Limit::perSecond(10);  
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,80 +10,73 @@ use App\Http\Controllers\FarmController;
 use App\Http\Controllers\SerreController;
 use App\Http\Controllers\StaffController;
 
+// Limiting request to 60 requests per minute
+Route::middleware(['throttle:api'])->group(function () {
 
-
-// limitting request to 60 per 1 minute
-Route::middleware(['throttle:40,1'])->group(function () {
-
-
-    // testing API 
+    // Testing API
     Route::get('hallo', function () {
-        return response()->json("Hello World", 201);
+        return response()->json("Hello World", 200);
     });
 
-
-    //Essentials API 
-    Route::post('register', [UserController::class, 'register']);                             //done
-    Route::post('login', [UserController::class, 'login']);                                   //done
-
+    // Essentials API
+    Route::post('register', [UserController::class, 'register']);                             // done
+    Route::post('login', [UserController::class, 'login']);                                   // done
 
     // Grouping inside Sanctum Middleware         
-        // User API 
-        
-        Route::get('user_is_welcomed_done/{id}', [UserController::class, 'UserIsWelcomedDone']); 
-        Route::get('users', [UserController::class, 'getAllUsers']); 
-        Route::get('usersNonAccepted', [UserController::class, 'getAllUsersNonAccepted']); 
-        Route::get('user', [UserController::class, 'getUser']); 
-        Route::get('getAdminIdFromUserId/{idUser}', [UserController::class, 'getAdminIdFromUserId']); 
-        Route::get('user/{id}', [UserController::class, 'getUserById']); 
-        Route::post('updateUserInfos/{idUser}', [UserController::class, 'updateUser']); 
-        Route::patch('user-type', [UserController::class, 'updateUserType']); 
-        Route::delete('deleteUserStaffNotAdmin/{id}', [UserController::class, 'deleteUserStaffNotAdmin']); 
-        Route::delete('deleteUserWhoIsAdmin/{id}', [UserController::class, 'deleteUserWhoIsAdmin']); 
-        Route::post('accept/{id}', [UserController::class, 'accepterUser']); 
-        Route::post('refuse/{id}', [UserController::class, 'refuserUser']); 
-        Route::get('updateUserRestriction/{id}/{access}', [UserController::class, 'updateUserRestriction']); 
-        Route::post('updatePassword/{id}', [UserController::class, 'updatePassword']); 
-        
-        // Admin API 
-            Route::get('admin', [AdminController::class, 'getAdmin']); 
-            Route::patch('admin', [AdminController::class, 'updateAdmin']); 
-            Route::delete('admin', [AdminController::class, 'deleteAdmin']); 
-            Route::get('admin/{idAdmin}', [AdminController::class, 'createAdmin']); 
+    // User API 
+    Route::get('user_is_welcomed_done/{id}', [UserController::class, 'UserIsWelcomedDone']); 
+    Route::get('users', [UserController::class, 'getAllUsers']); 
+    Route::get('usersNonAccepted', [UserController::class, 'getAllUsersNonAccepted']); 
+    Route::get('user', [UserController::class, 'getUser']); 
+    Route::get('getAdminIdFromUserId/{idUser}', [UserController::class, 'getAdminIdFromUserId']); 
+    Route::get('user/{id}', [UserController::class, 'getUserById']); 
+    Route::post('updateUserInfos/{idUser}', [UserController::class, 'updateUser']); 
+    Route::patch('user-type', [UserController::class, 'updateUserType']); 
+    Route::delete('deleteUserStaffNotAdmin/{id}', [UserController::class, 'deleteUserStaffNotAdmin']); 
+    Route::delete('deleteUserWhoIsAdmin/{id}', [UserController::class, 'deleteUserWhoIsAdmin']); 
+    Route::post('accept/{id}', [UserController::class, 'accepterUser']); 
+    Route::post('refuse/{id}', [UserController::class, 'refuserUser']); 
+    Route::get('updateUserRestriction/{id}/{access}', [UserController::class, 'updateUserRestriction']); 
+    Route::post('updatePassword/{id}', [UserController::class, 'updatePassword']); 
+    
+    // Admin API 
+    Route::get('admin', [AdminController::class, 'getAdmin']); 
+    Route::patch('admin', [AdminController::class, 'updateAdmin']); 
+    Route::delete('admin', [AdminController::class, 'deleteAdmin']); 
+    Route::get('admin/{idAdmin}', [AdminController::class, 'createAdmin']); 
 
-        // Staff API 
-            Route::post('staff', [StaffController::class, 'createStaff']); 
-            Route::delete('staff/{id}', [StaffController::class, 'deleteStaff']); 
-            Route::patch('staff/{id}/{type}', [StaffController::class, 'updateTypeStaff']); 
-            Route::get('staffs/{adminId}', [StaffController::class, 'getAllStaffs']); 
+    // Staff API 
+    Route::post('staff', [StaffController::class, 'createStaff']); 
+    Route::delete('staff/{id}', [StaffController::class, 'deleteStaff']); 
+    Route::patch('staff/{id}/{type}', [StaffController::class, 'updateTypeStaff']); 
+    Route::get('staffs/{adminId}', [StaffController::class, 'getAllStaffs']); 
 
-        // Farms API 
-        Route::post('farms', [FarmController::class, 'createFarm']); 
-        Route::get('farms/{id}', [FarmController::class, 'getAllFarmsPerAdmin']); 
-        Route::patch('farms/{id}', [FarmController::class, 'updateFarm']); 
-        Route::delete('farms/{id}', [FarmController::class, 'deleteFarm']); 
-        Route::get('farms/getSingleFarm/{id}', [FarmController::class, 'getSingleFarm']);  
-        Route::get('getFarmsWithGreenhouses/{id}', [FarmController::class, 'getFarmsWithGreenhouses']);  
+    // Farms API 
+    Route::post('farms', [FarmController::class, 'createFarm']); 
+    Route::get('farms/{id}', [FarmController::class, 'getAllFarmsPerAdmin']); 
+    Route::patch('farms/{id}', [FarmController::class, 'updateFarm']); 
+    Route::delete('farms/{id}', [FarmController::class, 'deleteFarm']); 
+    Route::get('farms/getSingleFarm/{id}', [FarmController::class, 'getSingleFarm']);  
+    Route::get('getFarmsWithGreenhouses/{id}', [FarmController::class, 'getFarmsWithGreenhouses']);  
 
-        // Serre API 
-        Route::post('serres', [SerreController::class, 'createSerre']); 
-        Route::get('serres-per-farm/{farmId}', [SerreController::class, 'getAllSerresPerFarm']); 
-        Route::patch('serres/{id}', [SerreController::class, 'updateSerre']); 
-        Route::delete('serres/{id}', [SerreController::class, 'deleteSerre']); 
+    // Serre API 
+    Route::post('serres', [SerreController::class, 'createSerre']); 
+    Route::get('serres-per-farm/{farmId}', [SerreController::class, 'getAllSerresPerFarm']); 
+    Route::patch('serres/{id}', [SerreController::class, 'updateSerre']); 
+    Route::delete('serres/{id}', [SerreController::class, 'deleteSerre']); 
 
-        // Prediction API 
-        Route::post('create_prediction', [PredictionController::class, 'createPrediction']);  //pending...
-        Route::get('predictions', [PredictionController::class, 'getAllPredictions']); 
-        Route::get('singlePrediction/{predId}', [PredictionController::class, 'getSinglePredictions']); 
-        Route::get('users/{userId}/predictions', [PredictionController::class, 'getUserPredictions']); 
-        Route::patch('predictions/{id}', [PredictionController::class, 'updatePrediction']); 
-        Route::delete('predictions/{id}', [PredictionController::class, 'deletePrediction']);
+    // Prediction API 
+    Route::post('create_prediction', [PredictionController::class, 'createPrediction']);  // pending...
+    Route::get('predictions', [PredictionController::class, 'getAllPredictions']); 
+    Route::get('singlePrediction/{predId}', [PredictionController::class, 'getSinglePredictions']); 
+    Route::get('users/{userId}/predictions', [PredictionController::class, 'getUserPredictions']); 
+    Route::patch('predictions/{id}', [PredictionController::class, 'updatePrediction']); 
+    Route::delete('predictions/{id}', [PredictionController::class, 'deletePrediction']);
 
-        // Image API
-        Route::post('images', [ImageController::class, 'createImage']);
-        Route::get('predictions/{predictionId}/images', [ImageController::class, 'getPredictionImages']);
-        Route::patch('images/{id}', [ImageController::class, 'updateImage']);
-        Route::delete('images/{id}', [ImageController::class, 'deleteImage']); 
- 
+    // Image API
+    Route::post('images', [ImageController::class, 'createImage']);
+    Route::get('predictions/{predictionId}/images', [ImageController::class, 'getPredictionImages']);
+    Route::patch('images/{id}', [ImageController::class, 'updateImage']);
+    Route::delete('images/{id}', [ImageController::class, 'deleteImage']); 
+
 });
-
